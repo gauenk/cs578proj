a1f3b939aa5154ff911186fd49e826d634a9c0f7
Kent Gauen <gauenk@purdue.edu>
2017-11-06 21:39:48 -0500

    added some comment

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ def k_fold_cross_validation(tr_data,tr_labels,split_number):
     # zero-one losses for naive bayes (nb) and support vector machine (svm)
     nb_zo_loss = []
     svm_zo_loss = []
+    svm_c1_zo_loss = []
+    svm_c2_zo_loss = []
+    svm_c3_zo_loss = []
 
     # grab what size the chunks should be
     chunk = int(len(tr_data)/split_number)
@@ -34,8 +37,8 @@ def k_fold_cross_validation(tr_data,tr_labels,split_number):
             = split_data(tr_data,tr_labels,split_number,i)
 
         ## add bias terms -- do we need it?
-        cv_tr_data = add_bias_term(cv_tr_data)
-        cv_te_data = add_bias_term(cv_te_data)
+        # cv_tr_data = add_bias_term(cv_tr_data)
+        # cv_te_data = add_bias_term(cv_te_data)
 
         ## NAIVE DECLARED HERE TO BE INIT-ed WITH THE DATA FOR CROSS VALIDATION
         nb = MultinomialNB()
@@ -44,18 +47,38 @@ def k_fold_cross_validation(tr_data,tr_labels,split_number):
         loss = zero_one_loss(preds,cv_te_labels)        
         nb_zo_loss += [loss]
         
-
         ## SVM DECLARED HERE TO BE INIT-ed WITH THE DATA FOR CROSS VALIDATION
-        svm = LinearSVC()
+        svm = LinearSVC(C=0.25,penalty='l2', loss='squared_hinge', dual=False)
         svm.fit(cv_tr_data,cv_tr_labels)
         preds = svm.predict(cv_te_data)
         loss = zero_one_loss(preds,cv_te_labels)        
         svm_zo_loss += [loss]
 
-    return np.array(nb_zo_loss),np.array(svm_zo_loss)
+        ## SVM_C1 DECLARED HERE TO BE INIT-ed WITH THE DATA FOR CROSS VALIDATION
+        svm_c1 = LinearSVC(C=10000,penalty='l2', loss='squared_hinge', dual=False)
+        svm_c1.fit(cv_tr_data,cv_tr_labels)
+        preds = svm_c1.predict(cv_te_data)
+        loss = zero_one_loss(preds,cv_te_labels)        
+        svm_c1_zo_loss += [loss]
+
+        ## SVM_C2 DECLARED HERE TO BE INIT-ed WITH THE DATA FOR CROSS VALIDATION
+        svm_c2 = LinearSVC(C=0.0001,penalty='l2', loss='squared_hinge', dual=False)
+        svm_c2.fit(cv_tr_data,cv_tr_labels)
+        preds = svm_c2.predict(cv_te_data)
+        loss = zero_one_loss(preds,cv_te_labels)        
+        svm_c2_zo_loss += [loss]
+
+        ## SVM_C3 DECLARED HERE TO BE INIT-ed WITH THE DATA FOR CROSS VALIDATION
+        svm_c3 = LinearSVC(C=1.0,penalty='l2', loss='squared_hinge', dual=False)
+        svm_c3.fit(cv_tr_data,cv_tr_labels)
+        preds = svm_c3.predict(cv_te_data)
+        loss = zero_one_loss(preds,cv_te_labels)        
+        svm_c3_zo_loss += [loss]
+
+    return np.array(nb_zo_loss),np.array(svm_zo_loss),np.array(svm_c1_zo_loss),np.array(svm_c2_zo_loss),np.array(svm_c3_zo_loss)
 
 def plot_experiment(nb_losses,svm_losses,xaxis_var,xlabel,cv_split_number,fn=None):
-    model_losses = {"nb":[nb_losses,"b--"],"svm":[svm_losses,"g--"]}
+    model_losses = {"nb":[nb_losses,"b--"],"svm: c = 0.25":[svm_losses,"o--"],"svm: c = 0.50":[svm_losses,"r--"],"svm: c = 0.75":[svm_losses,"k--"],"svm: c = 1.0":[svm_losses,"y--"]}
     model_lines = {}
     for model in model_losses.keys():
         if model_losses[model][0] is not None and len(model_losses[model][0]) != 0:
@@ -102,30 +125,39 @@ def experiment_1(data,labels,cv_split_number):
     tr_data_size = len(tr_data)
     nb_losses = []
     svm_losses = []
+    svm_c1_losses = []
+    svm_c2_losses = []
+    svm_c3_losses = []
     data_balance = [0.025,0.05,0.125,0.25]    
     for i in data_balance:
         # cross validation training data
         cv_tr_data = tr_data[:int(len(tr_data)*i),:1000]
         # cross validation labels
         cv_tr_labels = tr_labels[:int(len(tr_labels)*i)]
-        nb_loss,svm_loss = \
+        nb_loss,svm_loss,svm_c1_loss,svm_c2_loss,svm_c3_loss = \
             k_fold_cross_validation(cv_tr_data,cv_tr_labels,cv_split_number)
-        nb_losses += [nb_loss]
+        nb_losses += [nb_loss] # list of lists where each row is cv_split_number
         svm_losses += [svm_loss]
+        svm_c1_losses += [svm_c1_loss]
+        svm_c2_losses += [svm_c2_loss]
+        svm_c3_losses += [svm_c3_loss]
 
     model_losses = {"nb": nb_losses,
-                    "svm": svm_losses}
-
+                    "svm": svm_losses,
+                    "svm: c = 0.25": svm_c1_losses,
+                    "svm: c = 0.50": svm_c2_losses,
+                    "svm: c = 0.75": svm_c3_losses}
     write_losses(model_losses,data_balance,"exp1")
 
     return np.array(nb_losses),np.array(svm_losses),\
+        np.array(svm_c1_losses),np.array(svm_c2_losses),np.array(svm_c3_losses),\
         np.array(data_balance)*tr_data_size,\
         "Cross Validation Sample Size"
 
 
 if __name__ == "__main__":
 
-    if True:
+    if False:
         test_model()
     else:
         if len(sys.argv) < 3:
@@ -141,7 +173,7 @@ if __name__ == "__main__":
             no_examples = 50
 
         if exp_no == "1" or exp_no == "-1":
-            nb_losses,svm_losses,xaxis_var,xlabel =\
+            nb_losses,svm_losses,svm_c1_losses,svm_c2_losses,svm_c3_losses,xaxis_var,xlabel =\
                    experiment_1(tr_data,tr_labels,cv_split_number)
-            plot_experiment(dt_losses,bt_losses,rf_losses,svm_losses,\
+            plot_experiment(nb_losses,svm_losses,\
                             xaxis_var,xlabel,cv_split_number,"./figures/exp_1.pdf")
